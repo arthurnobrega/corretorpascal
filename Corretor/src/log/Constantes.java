bb7fef38c3b3ecd04582fc19f9e17e62545dc563
arthurthiago <arthurthiago@7a904b56-7638-0410-9fcd-4bb9978d2467>
2007-08-24 20:06:21 +0000

    * Atualização - Arquivos migrados.

---
+++ Corretor/src/log/Constantes.java
@@ -0,0 +1,11 @@
+package br.unb.cic.corretor.gui;
+
+/**
+ * Classe que é utilizada para armazenar as constantes que serão chamadas no
+ * decorrer do programa, tanto quando ocorrer um erro e mostrar os dados
+ * para o usuário, quanto para pegar os nomes de arquivos padrões do programa.
+ *
+ */
+public class Constantes {
+
+}
---
+++ Corretor/src/src/Arquivos.java
@@ -0,0 +1,45 @@
+package br.unb.cic.corretor.gui;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import javax.swing.JOptionPane;
+
+public class Arquivos {
+	
+	/** 
+	 * Este método retorna o texto que está contido em um determinado arquivo
+	 * na forma de String. 
+	 * @param filename O Nome do Arquivo.
+	 * @return Uma String que contém o texto do arquivo.
+	 */
+	public static String getTextoArquivo(String nomeArquivo) throws IOException {
+            StringBuffer buffer = new StringBuffer();
+                    BufferedReader in = new BufferedReader (new InputStreamReader (
+                                    new FileInputStream (nomeArquivo), "UTF-8"));
+            String line;
+            while((line = in.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+            in.close();
+            return buffer.toString();
+	}
+	
+	/** 
+	 * Este método grava os dados que são passados dentro do arquivo e o fecha.
+	 * @param caminhoArquivo O caminho completo do arquivo no qual será gravado os dados.
+	 * @param texto O texto que será gravado no arquivo.
+	 */
+	public static void salvarArquivo(String caminhoArquivo, String texto) throws IOException {
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+                            new FileOutputStream(caminhoArquivo), "UTF8"));
+            out.write(texto);
+            out.close();
+	}
+
+}
