1890ff86de55fbaa590c94faa0f6feb27ce6244b
arthurthiago <arthurthiago@7a904b56-7638-0410-9fcd-4bb9978d2467>
2007-09-28 14:06:51 +0000

    *  GerenciaSerializacao.java - Adicionada esta classe para gerenciar a serializacao do vetor de PastaCorrecao.
    * Executador.java
    - Modificada para testar a execução no linux.

---
+++ Corretor/src/log/GerenciaSerializacao.java
@@ -0,0 +1,37 @@
+/*
+ * GerenciaSerializacao.java
+ *
+ * Created on 28 de Setembro de 2007, 10:35
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package log;
+
+import java.io.File;
+import src.Arquivos;
+import src.PastaCorrecao;
+
+/**
+ *
+ * @author e0631205
+ */
+public class GerenciaSerializacao {
+    
+    File diretorio = null;
+    
+    /** Creates a new instance of GerenciaSerializacao */
+    public GerenciaSerializacao(File diretorio) {
+        this.diretorio = diretorio;
+    }
+    
+    public void serializar(PastaCorrecao[] pastasCorrecao) {
+        Arquivos.serializarCorrecao(diretorio, pastasCorrecao);
+    }
+    
+    public PastaCorrecao[] desserializar() {
+        return Arquivos.desserializarCorrecao(diretorio);
+    }
+    
+}
