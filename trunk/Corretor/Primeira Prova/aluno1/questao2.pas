928168099751ca58ef004bf650b90e7a5a2016e4
arthurthiago <arthurthiago@7a904b56-7638-0410-9fcd-4bb9978d2467>
2007-12-01 21:17:38 +0000

    * Modificando as questões

--- trunk/Corretor/Primeira Prova/aluno1/questao2.pas
+++ trunk/Corretor/Primeira Prova/aluno1/questao2.pas
@@ -1,44 +1,26 @@
-Program EncontraMaior;
-
-Var
-    nome:  array[1..3] of string[10];
-    idade: array[1..3] of integer;
-    maiorNome, menorNome: string[10];
-    maiorIdade, menorIdade, i: integer;
-Begin
-    { Lê os dados da tela. }
-    for i := 1 to 3 do
-    begin
-        write('Nome ', i, ': ');
-        readln(nome[i]);
-        write('Idade ', i, ': ');
-        readln(idade[i]);
-    end;
-    writeln;
-    
-    { Encontra o maior e o menor. }
-    maiorIdade := idade[1];
-    menorIdade := idade[1];
-    maiorNome := nome[1];
-    menorNome := nome[1];
-    for i := 2 to 3 do
-    begin
-        if (idade[i] > maiorIdade) then
-        begin
-            maiorIdade := idade[i];
-            maiorNome := nome[i];
-        end
-        else if (idade[i] < menorIdade) then
-        begin
-            menorIdade := idade[i];
-            menorNome := nome[i];
-        end;
-    end;
-    
-    { Escreve o resultado na tela. }
-    writeln('A pessoa mais velha é ', maiorNome);
-    writeln('. Sua idade é ', maiorIdade);
-    writeln('A pessoa mais nova é ', menorNome);
-    writeln('. Sua idade é ', menorIdade);
-    writeln;
-End.
+Program Transito;
+Var 
+	tipo, dia, desconto : integer;
+	valor, multa : real;
+begin
+	readln (tipo);
+	readln (dia);
+	if (tipo = 1)
+		then valor := 100
+		else if (tipo = 2)
+					then  valor := 200
+					else  valor := 500;
+	if  (dia <= 5)
+		then desconto := 25
+	else  if  (dia <= 10)
+		then desconto := 20
+	else  if  (dia <= 15)
+		then desconto := 15
+	else  if  (dia <= 20)
+		then desconto := 10
+	else  if  (dia <= 25)
+		then desconto := 5
+		else desconto := 0;
+	multa := valor * ( 1 - desconto/100);
+	writeln (multa);
+end.
